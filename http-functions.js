08fa2cf332da5b82b1864686233cf579dad755de
Kathleen pearce <kathleenpearce@live.com>
2019-04-08 20:35:13 +0000

    step 5 with test

--- http-functions.js
+++ http-functions.js
@@ -1,8 +1,8 @@
-function getHTML (options, callback) {
-var https = require('https');
+module.exports = function getHTML (options, callback) {
+ var https = require('https');
 
   https.get(options, function(response) {
-    var data = "";
+      var data = "";
       response.setEncoding('utf8');
       response.on('data', function(chunk) {
         if (response.statusCode !== 200){
@@ -17,12 +17,3 @@ var https = require('https');
   });
 }
 
-function printHTML (html) {
-console.log(html);}
-
-var requestOptions = {
-  host: 'sytantris.github.io',
-  path: '/http-examples/step4.html'
-};
-
-getHTML(requestOptions, printHTML);
