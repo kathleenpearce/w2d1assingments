0181f62f9b58812afe418d9f6c2ad91297195d0b
Kathleen pearce <kathleenpearce@live.com>
2019-04-08 18:48:20 +0000

    step 2

---
+++ BuffString.js
@@ -0,0 +1,28 @@
+function getAndPrintHTML() {
+var https = require('https');
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function(response) {
+      response.setEncoding('utf8');
+
+      var data = "";
+      response.on('data', function(data) {
+        if (response.statusCode !== 200){
+           console.log('Response Failed' + statusCode);
+         } else {
+          data += data;
+            console.log(data);
+          }
+      });
+  })
+};
+
+
+
+
+
+getAndPrintHTML();
