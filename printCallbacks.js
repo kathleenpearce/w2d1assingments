f8c733fd5c43848c66b42dcd3a13903ec78bfdd4
Kathleen pearce <kathleenpearce@live.com>
2019-04-08 19:53:19 +0000

    step 4

--- printCallbacks.js
+++ printCallbacks.js
@@ -1,14 +1,35 @@
 function getHTML (options, callback) {
 
-  /* Add your code here */
 
+var options = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step4.html'
+  };
+
+var https = require('https');
+var data = "";
+
+  https.get(options, function(response) {
+      response.setEncoding('utf8');
+      response.on('data', function(chunk) {
+        if (response.statusCode !== 200){
+           console.log('Response Failed' + statusCode);
+        } else {
+          data += chunk;
+      };
+      response.on('end', function(){
+        printHTML(data);
+        });
+      });
+  });
 }
 
 function printHTML (html) {
-  console.log(html);
-}
+console.log(html);}
 
 var requestOptions = {
   host: 'sytantris.github.io',
   path: '/http-examples/step4.html'
 };
+
+getHTML();
