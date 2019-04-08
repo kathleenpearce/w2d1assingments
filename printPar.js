619fc2f8632c337d013540fd6c15635614a92cbd
Kathleen pearce <kathleenpearce@live.com>
2019-04-08 19:03:50 +0000

    step 3

---
+++ printPar.js
@@ -0,0 +1,30 @@
+function getAndPrintHTML(options) {
+
+  var options = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step3.html'
+  };
+
+var https = require('https');
+
+  https.get(options, function(response) {
+      response.setEncoding('utf8');
+
+      var data = "";
+      response.on('data', function(data) {
+        if (response.statusCode !== 200){
+           console.log('Response Failed' + statusCode);
+         } else {
+          data += data;
+            console.log(data);
+          }
+      });
+  })
+};
+
+
+getAndPrintHTML();
+// var requestOptions = {
+//     host: 'sytantris.github.io',
+//     path: '/http-examples/step1.html'
+//   };
